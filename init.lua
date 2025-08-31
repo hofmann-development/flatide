1b2da75af9099629b7d1668f92e076390eaa1d94
Stefan Hofmann <stefan@hofmann-development.com>
2025-08-31 21:55:07 +0200

    Fixed init.lua

--- init.lua
+++ init.lua
@@ -20,5 +20,3 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
-vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
